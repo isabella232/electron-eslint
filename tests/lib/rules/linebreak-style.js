644dafa41e13902f9837e5abd079181c23e7381a
Paul Betts <paul@paulbetts.org>
2017-01-01 13:12:36 -0800

    Remove linebreak-style, see https://github.com/eslint/eslint/pull/7823

--- tests/lib/rules/linebreak-style.js
+++ tests/lib/rules/linebreak-style.js
@@ -11,9 +11,6 @@
 const rule = require("../../../lib/rules/linebreak-style"),
     RuleTester = require("../../../lib/testers/rule-tester");
 
-const EXPECTED_LF_MSG = "Expected linebreaks to be 'LF' but found 'CRLF'.",
-    EXPECTED_CRLF_MSG = "Expected linebreaks to be 'CRLF' but found 'LF'.";
-
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
@@ -22,94 +19,7 @@ const ruleTester = new RuleTester();
 
 ruleTester.run("linebreak-style", rule, {
 
-    valid: [
-        {
-            code: "var a = 'a',\n b = 'b';\n\n function foo(params) {\n /* do stuff */ \n }\n",
-            args: [2]
-        },
-        {
-            code: "var a = 'a',\n b = 'b';\n\n function foo(params) {\n /* do stuff */ \n }\n",
-            options: ["unix"]
-        },
-        {
-            code: "var a = 'a',\r\n b = 'b';\r\n\r\n function foo(params) {\r\n /* do stuff */ \r\n }\r\n",
-            options: ["windows"]
-        },
-        {
-            code: "var b = 'b';",
-            options: ["unix"]
-        },
-        {
-            code: "var b = 'b';",
-            options: ["windows"]
-        }
-    ],
+    valid: [],
 
-    invalid: [
-        {
-            code: "var a = 'a';\r\n",
-            output: "var a = 'a';\n",
-            args: [2],
-            errors: [{
-                line: 1,
-                column: 13,
-                message: EXPECTED_LF_MSG
-            }]
-        },
-        {
-            code: "var a = 'a';\r\n",
-            output: "var a = 'a';\n",
-            options: ["unix"],
-            errors: [{
-                line: 1,
-                column: 13,
-                message: EXPECTED_LF_MSG
-            }]
-        },
-        {
-            code: "var a = 'a';\n",
-            output: "var a = 'a';\r\n",
-            options: ["windows"],
-            errors: [{
-                line: 1,
-                column: 13,
-                message: EXPECTED_CRLF_MSG
-            }]
-        },
-        {
-            code: "var a = 'a',\n b = 'b';\n\n function foo(params) {\r\n /* do stuff */ \n }\r\n",
-            output: "var a = 'a',\n b = 'b';\n\n function foo(params) {\n /* do stuff */ \n }\n",
-            args: [2],
-            errors: [{
-                line: 4,
-                column: 24,
-                message: EXPECTED_LF_MSG
-            },
-            {
-                line: 6,
-                column: 3,
-                message: EXPECTED_LF_MSG
-            }]
-        },
-        {
-            code: "var a = 'a',\r\n b = 'b';\r\n\n function foo(params) {\r\n\n /* do stuff */ \n }\r\n",
-            output: "var a = 'a',\r\n b = 'b';\r\n\r\n function foo(params) {\r\n\r\n /* do stuff */ \r\n }\r\n",
-            options: ["windows"],
-            errors: [{
-                line: 3,
-                column: 1,
-                message: EXPECTED_CRLF_MSG
-            },
-            {
-                line: 5,
-                column: 1,
-                message: EXPECTED_CRLF_MSG
-            },
-            {
-                line: 6,
-                column: 17,
-                message: EXPECTED_CRLF_MSG
-            }]
-        }
-    ]
+    invalid: []
 });
